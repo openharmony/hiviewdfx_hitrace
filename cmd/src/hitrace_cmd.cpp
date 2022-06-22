4e2d6b97060d1ce47eda3f7bb26ab46ec73036e5
wangyang2022 <wangyang412@huawei.com>
2022-06-22 14:33:57 +0800

    add dms trace tag Signed-off-by: wangyang2022 <wangyang412@huawei.com> Change-Id: Ie916144f6e114bb7553a7b0cd55e8c9b53d9eda9

--- cmd/src/hitrace_cmd.cpp
+++ cmd/src/hitrace_cmd.cpp
@@ -865,8 +865,7 @@ static void InitAllSupportTags()
     g_tagMap["dhfwk"] = { "dhfwk", "Distributed Hardware FWK", HITRACE_TAG_DISTRIBUTED_HARDWARE_FWK, USER, {}};
     g_tagMap["dscreen"] = { "dscreen", "Distributed Screen", HITRACE_TAG_DISTRIBUTED_SCREEN, USER, {}};
     g_tagMap["devicemanager"] = { "devicemanager", "Device Manager", HITRACE_TAG_DEVICE_MANAGER, USER, {}};
-    g_tagMap["distributedschedule"] = { "distributedschedule", "Distributed Schedule",
-        HITRACE_TAG_DISTRIBUTED_SCHEDULE, USER, {}};
+    g_tagMap["dsched"] = { "dsched", "Distributed Schedule", HITRACE_TAG_DISTRIBUTED_SCHEDULE, USER, {}};
     g_tagMap["samgr"] = { "samgr", "samgr", HITRACE_TAG_SAMGR, USER, {}};
     g_tagMap["app"] = { "app", "APP Module", HITRACE_TAG_APP, USER, {}};
     g_tagMap["dcamera"] = { "dcamera", "Distributed Camera", HITRACE_TAG_DISTRIBUTED_CAMERA, USER, {}};
