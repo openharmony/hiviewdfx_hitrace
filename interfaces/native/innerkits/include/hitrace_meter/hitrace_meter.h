164377617c263af5eb867b56d205191f85c0231f
shipengtao9579 <shipengtao@huawei.com>
2022-07-29 19:05:01 +0800

    add useriam tag
    Signed-off-by: shipengtao9579 <shipengtao@huawei.com>
    Change-Id: Id7d30b5ff39ebeb5a0223e15878fe5353091dd59

--- interfaces/native/innerkits/include/hitrace_meter/hitrace_meter.h
+++ interfaces/native/innerkits/include/hitrace_meter/hitrace_meter.h
@@ -24,6 +24,7 @@ extern "C" {
 
 constexpr uint64_t HITRACE_TAG_NEVER = 0; // This tag is never enabled.
 constexpr uint64_t HITRACE_TAG_ALWAYS = (1ULL << 0); // This tag is always enabled.
+constexpr uint64_t HITRACE_TAG_USERIAM = (1ULL << 26); // This tag is useriam.
 constexpr uint64_t HITRACE_TAG_DISTRIBUTED_AUDIO = (1ULL << 27); // Distributed audio tag.
 constexpr uint64_t HITRACE_TAG_DLSM = (1ULL << 28); // device security level tag.
 constexpr uint64_t HITRACE_TAG_FILEMANAGEMENT = (1ULL << 29); // filemanagement tag.
