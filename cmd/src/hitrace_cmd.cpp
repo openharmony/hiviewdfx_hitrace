3f57fcf024677c58eda33cc962c100b49bca5384
jiahaoluo <luojiahao5@huawei.com>
2022-07-30 16:50:40 +0800

    add user tag for filemanagement
    Signed-off-by: jiahaoluo <luojiahao5@huawei.com>
    Change-Id: Ibf4f40118749b6b9c970dbfec7a0f1ec5776ae61

--- cmd/src/hitrace_cmd.cpp
+++ cmd/src/hitrace_cmd.cpp
@@ -886,7 +886,7 @@ static void InitAllSupportTags()
     g_tagMap["gresource"] = { "gresource", "Global Resource Manager", HITRACE_TAG_GLOBAL_RESMGR, USER, {}};
     g_tagMap["power"] = { "power", "Power Manager", HITRACE_TAG_POWER, USER, {}};
     g_tagMap["bluetooth"] = { "bluetooth", "communicatio bluetooth", HITRACE_TAG_BLUETOOTH, USER, {}};
-    g_tagMap["filemanagement"] = { "filemanagement", "filemanagement", HITRACE_TAG_FILEMANAGEMENT, {}};
+    g_tagMap["filemanagement"] = { "filemanagement", "filemanagement", HITRACE_TAG_FILEMANAGEMENT, USER, {}};
     g_tagMap["dslm"] = { "dslm", "device security level", HITRACE_TAG_DLSM, {}};
 
     // Kernel os
