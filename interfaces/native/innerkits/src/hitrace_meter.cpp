3de34ada5447b2aec20d07bd8bdd7b8d0b073e17
zyxzyx <zhangyixin19@huawei.com>
2023-09-13 14:59:19 +0800

    Update GetSysParamTags
    Signed-off-by: zyxzyx <zhangyixin19@huawei.com>

--- interfaces/native/innerkits/src/hitrace_meter.cpp
+++ interfaces/native/innerkits/src/hitrace_meter.cpp
@@ -110,10 +110,9 @@ uint64_t GetSysParamTags()
         if (changed == 1) {
             HiLog::Info(LABEL, "g_tagsProperty changed, previous is %{public}s.", to_string(g_tagsProperty.load()).c_str());
             tags = strtoull(paramValue, nullptr, 0);
-            g_tagsProperty = (tags | HITRACE_TAG_ALWAYS) & HITRACE_TAG_VALID_MASK;
-            HiLog::Info(LABEL, "g_tagsProperty changed, now is %{public}s.", to_string(g_tagsProperty.load()).c_str());
+        } else {
+            return g_tagsProperty;
         }
-        return g_tagsProperty;
     }
 
     IsAppValid();
@@ -202,7 +201,7 @@ void AddHitraceMeterMarker(MarkerType type, uint64_t tag, const std::string& nam
         }
         std::call_once(g_onceFlag, OpenTraceMarkerFile);
     }
-    GetSysParamTags();
+    g_tagsProperty = GetSysParamTags();
     if (UNEXPECTANTLY(g_tagsProperty & tag) && g_markerFd != -1) {
         // record fomart: "type|pid|name value".
         char buf[BUFFER_LEN] = {0};
