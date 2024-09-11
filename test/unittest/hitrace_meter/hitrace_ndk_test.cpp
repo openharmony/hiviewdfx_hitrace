67b71ac82aa22d85e198dc866e45857efdc5845c
Federer-JC <jincong4@huawei.com>
2024-09-11 17:26:14 +0800

    delete invalid expect result. Signed-off-by:Federer-JC <jincong4@huawei.com>
    Signed-off-by: Federer-JC <jincong4@huawei.com>

--- test/unittest/hitrace_meter/hitrace_ndk_test.cpp
+++ test/unittest/hitrace_meter/hitrace_ndk_test.cpp
@@ -1612,23 +1612,17 @@ HWTEST_F(HitraceNDKTest, HitraceRomTest001, TestSize.Level1)
 HWTEST_F(HitraceNDKTest, BytraceRomTest001, TestSize.Level1)
 {
     uint64_t realSize = 0;
-    int bytes = 1024;
     for (int i = 0; i < BYTRACE_OUTPUT_PATH.size(); i++) {
         struct stat st = {0};
         if (BYTRACE_OUTPUT_PATH[i].find(BYTRACE_LINK_PATH) != std::string::npos) {
             lstat(BYTRACE_LINK_PATH, &st);
-            realSize += static_cast<uint64_t>(st.st_size * bytes);
+            realSize += static_cast<uint64_t>(st.st_size);
         } else {
             stat(BYTRACE_OUTPUT_PATH[i].c_str(), &st);
             realSize += static_cast<uint64_t>(st.st_size);
         }
     }
-
-    if (realSize > BYTRACE_BASELINE_SIZE) {
-        std::cout << "realSize > BYTRACE_BASELINE_SIZ" << std::endl;
-    } else {
-        EXPECT_LT(realSize, BYTRACE_BASELINE_SIZE);
-    }
+    EXPECT_LT(realSize, BYTRACE_BASELINE_SIZE);
     std::cout << "realSize: " << realSize << std::endl;
 }
 } // namespace HitraceTest
