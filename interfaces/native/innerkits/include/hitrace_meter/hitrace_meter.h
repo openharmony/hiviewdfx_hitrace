d1930c048ad04a63d4259813c2872ce9e97d6723
fengjq <fengjunqing1@huawei.com>
2025-01-18 22:17:00 +0800

    Remove cloud file hitrace tag
    Signed-off-by: fengjq <fengjunqing1@huawei.com>

--- interfaces/native/innerkits/include/hitrace_meter/hitrace_meter.h
+++ interfaces/native/innerkits/include/hitrace_meter/hitrace_meter.h
@@ -29,7 +29,6 @@ constexpr uint64_t HITRACE_TAG_NEVER = 0; // This tag is never enabled.
 constexpr uint64_t HITRACE_TAG_ALWAYS = (1ULL << 0); // This tag is always enabled.
 constexpr uint64_t HITRACE_TAG_DRM = (1ULL << 6); // Media subsystem Digital Rights Management tag.
 constexpr uint64_t HITRACE_TAG_SECURITY = (1ULL << 7); // Security subsystem tag.
-constexpr uint64_t HITRACE_TAG_CLOUD_FILE = (1ULL << 8); // Cloud file system tag.
 constexpr uint64_t HITRACE_TAG_ANIMATION = (1ULL << 9); // Animation tag.
 constexpr uint64_t HITRACE_TAG_PUSH = (1ULL << 10); // Push subsystem tag.
 constexpr uint64_t HITRACE_TAG_VIRSE = (1ULL << 11); // virtualization service.
