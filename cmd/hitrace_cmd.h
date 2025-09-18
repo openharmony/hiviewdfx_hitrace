868f766e01c9aeb64268b4f90f5d36ea1ba6544f
gan-chuantao <ganchuantao1@h-partners.com>
2025-09-18 16:42:52 +0800

    大函数整改
    Signed-off-by: gan-chuantao <ganchuantao1@h-partners.com>
    Change-Id: I3e310cac2a8bdd14c29fcfc29764f60d739a40f6

---
+++ cmd/hitrace_cmd.h
@@ -0,0 +1,96 @@
+#ifndef HITRACE_CMD_DEFINE_H
+#define HITRACE_CMD_DEFINE_H
+
+struct TraceArgs {
+    std::string tags;
+    std::string tagGroups;
+    std::string clockType;
+    std::string level;
+    int bufferSize = 0;
+    int fileSize = 0;
+    bool overwrite = true;
+    std::string output;
+
+    int duration = 0;
+    bool isCompress = false;
+};
+
+struct TraceSysEventParams {
+    std::string opt;
+    std::string caller;
+    std::string tags;
+    int duration = 0;
+    int bufferSize = 0;
+    int fileSize = 0;
+    int fileLimit = 0;
+    std::string clockType;
+    bool isCompress = false;
+    bool isRaw = false;
+    bool isOverwrite = true;
+    int errorCode = 0;
+    std::string errorMessage;
+};
+
+enum RunningState {
+    /* Initial value */
+    STATE_NULL = 0,
+
+    /* Record a short trace */
+    RECORDING_SHORT_TEXT = 1, // --text
+    RECORDING_SHORT_RAW = 2,  // --raw
+
+    /* Record a long trace */
+    RECORDING_LONG_BEGIN = 10,         // --trace_begin
+    RECORDING_LONG_DUMP = 11,          // --trace_dump
+    RECORDING_LONG_FINISH = 12,        // --trace_finish
+    RECORDING_LONG_FINISH_NODUMP = 13, // --trace_finish_nodump
+    RECORDING_LONG_BEGIN_RECORD = 14,  // --trace_begin --record
+    RECORDING_LONG_FINISH_RECORD = 15, // --trace_finish --record
+
+    /* Manipulating trace services in snapshot mode */
+    SNAPSHOT_START = 20, // --start_bgsrv
+    SNAPSHOT_DUMP = 21,  // --dump_bgsrv
+    SNAPSHOT_STOP = 22,  // --stop_bgsrv
+
+    /* Help Info */
+    SHOW_HELP = 31,          // -h, --help
+    SHOW_LIST_CATEGORY = 32, // -l, --list_categories
+
+    /* Set system parameter */
+    SET_TRACE_LEVEL = 33,    // --trace_level level
+    GET_TRACE_LEVEL = 34,    // --trace_level
+};
+
+enum CmdErrorCode {
+    OPEN_ROOT_PATH_FAILURE = 2001,
+    OPEN_FILE_PATH_FAILURE = 2002,
+    TRACING_ON_CLOSED = 2003,
+};
+
+using CommandFunc = std::function<bool(const RunningState)>;
+using TaskFunc = std::function<bool()>;
+
+static bool HandleRecordingShortRaw();
+static bool HandleRecordingShortText();
+static bool HandleRecordingLongBegin();
+static bool HandleRecordingLongDump();
+static bool HandleRecordingLongFinish();
+static bool HandleRecordingLongFinishNodump();
+static bool HandleRecordingLongBeginRecord();
+static bool HandleRecordingLongFinishRecord();
+static bool HandleOpenSnapshot();
+static bool HandleDumpSnapshot();
+static bool HandleCloseSnapshot();
+static bool SetTraceLevel();
+static bool GetTraceLevel();
+
+static bool HandleOptBuffersize(const RunningState& setValue);
+static bool HandleOptTraceclock(const RunningState& setValue);
+static bool HandleOptTime(const RunningState& setValue);
+static bool HandleOptOutput(const RunningState& setValue);
+static bool HandleOptOverwrite(const RunningState& setValue);
+static bool HandleOptRecord(const RunningState& setValue);
+static bool HandleOptFilesize(const RunningState& setValue);
+static bool HandleOptTracelevel(const RunningState& setValue);
+static bool SetRunningState(const RunningState& setValue);
+#endif
